fd3d0b46d71da7e0987edab805f7a1c04cc17625
imnotvaibhav <vaibhavsemwal007@gmail.com>
2024-08-20 22:20:56 +0530

    added lines in Table.py

--- Table.py
+++ Table.py
@@ -1,7 +1,10 @@
 # Table of 9
 def table(n=9):
+    print("---------------")
     for i in range(1,11):
         print(n,"*",i,"=",n*i)
+    print("---------------")
+        
     return None
 
 if __name__ == "__main__":
